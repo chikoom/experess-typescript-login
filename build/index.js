6ad6e76a0a5e6c6e9a23d81c019c063b4569a824
Chikoom <callforcopy@gmail.com>
2020-09-20 10:58:19 +0300

    dotenv config

--- build/index.js
+++ build/index.js
@@ -1,2 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+require('dotenv').config();
+var app = express_1.default();
+app.get('/', function (req, res) {
+    res.send("\n    <div> \n      <h1> Hello Express </h1> \n    </div>\n  ");
+});
+var PORT = process.env.PORT;
+app.listen(PORT, function () {
+    console.log("Server UP! listen on PORT : " + PORT);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div> 
+      <h1> Hello Express </h1> 
+    </div>
+  `)
+})
+
+const { PORT } = process.env
+app.listen(PORT, () => {
+  console.log(`Server UP! listen on PORT : ${PORT}`)
+})
